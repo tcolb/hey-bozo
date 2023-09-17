ea9d2491ca5a5566b78bbee1b081f6e360fe620e
Tristan Colby <tristan.colb@gmail.com>
2023-09-17 16:56:21 -0700

    Before silence refactor

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,58 @@
-use std::fs::OpenOptions;
-use std::mem::MaybeUninit;
-use std::time::{Instant, Duration};
-use std::{env, collections::HashMap, sync::Arc};
-use dasp_signal::{Signal, interpolate};
-use dasp_frame::Frame;
+use std::collections::VecDeque;
+use std::{env, sync::Arc};
+use cheetah::{CheetahBuilder, Cheetah};
 use porcupine::{PorcupineBuilder, Porcupine};
 use dotenv::dotenv;
-use ringbuf::{Consumer, Producer, SharedRb};
+
 use serenity::prelude::TypeMap;
 use serenity::{client::{Client, Context, EventHandler}, 
                framework::{StandardFramework, standard::{macros::{group, command}, Args, CommandResult}},
                prelude::{GatewayIntents, Mentionable, TypeMapKey},
                model::{gateway::Ready, prelude::{Message, ChannelId}}};
+use songbird::events::context_data::SpeakingUpdateData;
 use songbird::{EventHandler as VoiceEventHandler, EventContext, model::payload::{Speaking, ClientDisconnect}, Event, Config, driver::DecodeMode, SerenityInit, CoreEvent};
 use async_trait::async_trait;
 use tokio::sync::RwLock;
 use wav::WAV_FORMAT_PCM;
 use tokio::sync::mpsc;
 
-#[tokio::main]
+use rubato::{Resampler, SincFixedOut, SincInterpolationType, SincInterpolationParameters, WindowFunction};
+
+async fn read_frames(rx: &mut mpsc::Receiver<(u32, Vec<i16>)>, buf: &mut VecDeque<i16>, instant: &mut std::time::Instant, n: usize, channels: usize) -> Vec<Vec<f64>> {
+
+    let mut out = Vec::with_capacity(channels);
+    for _ in 0..channels {
+        out.push(Vec::with_capacity(n));
+    }
+
+    while buf.len() < n * 2 {
+        let (_id, packet_audio) = rx.recv().await.unwrap();
+        buf.extend(packet_audio);
+    }
+    
+    for _ in 0..n {
+        let l = buf.pop_front().unwrap() as f64 / 32768.0;
+        let r = buf.pop_front().unwrap() as f64 / 32768.0;
+        out[0].push(l.clamp(-1.0, 1.0));
+        out[1].push(r.clamp(-1.0, 1.0));
+    }
+
+    out
+}
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() {
     println!("Hello, world!");
     dotenv().ok();
     
     let mut client = init_serenity().await;
 
-    let (tx_48khz, mut rx_48khz) = mpsc::channel::<Consumer<i16, Arc<SharedRb<i16, Vec<MaybeUninit<i16>>>>>>(32);
+    //let audio_rb = ringbuf::HeapRb::<i32>::new(64);
 
+    let (tx_48khz, mut rx_48khz) = mpsc::channel::<(u32, Vec<i16>)>(32);
     {
         let mut guard: tokio::sync::RwLockWriteGuard<'_, serenity::prelude::TypeMap> = client.data.write().await;
-        guard.insert::<DiscordHandles>(DiscordHandles {
-            map: HashMap::default(),
+        guard.insert::<SharedState>(SharedState {
             tx: tx_48khz
         });
     };
@@ -39,110 +61,117 @@ async fn main() {
         client.start().await.expect("Error in serenity client!");
     });
 
-    let sample_rate = 16_000;
-    let frame_length = 512 as usize;
-
-    let (tx_16khz, mut rx_16khz) = mpsc::channel::<Consumer<i16, Arc<SharedRb<i16, Vec<MaybeUninit<i16>>>>>>(32);
-
-    let downsampler_handle = tokio::spawn(async move {
-        loop {
-            let mut cons_48khz = rx_48khz.recv().await.unwrap();
-            // Create ring buffer for 16khz audio
-            let (mut prod_16khz, cons_16khz) = ringbuf::HeapRb::<i16>::new(4 * 16_000).split();
-            // Send consumer off to porcupine thread
-            tx_16khz.send(cons_16khz).await.unwrap();
-
-            tokio::spawn(async move {
-                loop {
-                    if cons_48khz.len() >= frame_length {
-                        let mut from_signal = dasp_signal::from_interleaved_samples_iter::<_, [i16; 2]>(cons_48khz.pop_iter());
-    
-                        let left = from_signal.next();
-                        let right = from_signal.next();
-                        let interpolator = dasp_interpolate::linear::Linear::new(left, right);
-                        let mut converter = interpolate::Converter::from_hz_to_hz(
-                            from_signal,
-                            interpolator,
-                            48_000.0,
-                            sample_rate as f64
-                        );
-    
-                        loop {
-                            let frame = converter.next();
-                            if converter.is_exhausted() {
-                            //if frame == <[i16; 2]>::EQUILIBRIUM {
-                                break;
-                            }
-                            prod_16khz.push((frame[0] + frame[1]) / 2).unwrap();
-                            //prod_16khz.push(frame[0]).unwrap();
-                            //prod_16khz.push(frame[1]).unwrap();
-                        }
-                    }
-                }
-            });
-        }
-    });
-
-    let porcupine_handle = tokio::spawn(async move {
-        loop {
-            let mut cons_16khz = rx_16khz.recv().await.unwrap();
-
-            tokio::spawn(async move {
-                // Porcupine model per voice, should probably mix audio beforehand
-                let porcupine = init_porcupine();
-                let mut input_frame = Vec::<i16>::new();
-
-                let mut from_signal = dasp_signal::from_interleaved_samples_iter::<_, [i16; 2]>(cons_48khz.pop_iter());
-                let left = from_signal.next();
-                let right = from_signal.next();
-                let interpolator = dasp_interpolate::linear::Linear::new(left, right);
-                let mut converter = interpolate::Converter::from_hz_to_hz(
-                    from_signal,
-                    interpolator,
-                    48_000.0,
-                    sample_rate as f64
-                );
-
-                loop {
-                    if converter.is_exhausted() {
-                        continue;
-                    }
-                    let frame = converter.next();
-                    input_frame.push((frame[0] + frame[1]) / 2);
-
-                    if input_frame.len() == frame_length {
-                        if let Ok(index) = porcupine.process(&input_frame) {
-                            if index >= 0 {
-                                // we out here gamers
-                                println!("Hey bozo detected!");
-                                //let mut write_guard = data.write().await;
-                                //let buffers = write_guard.get_mut::<VoiceBuffers>().unwrap();
-                            }
-                        }
-                        input_frame.clear();
-                    }
-
-                    assert!(input_frame.len() < frame_length);
-                }
-                    // if frame.len() >= 16_000 * 2 {
-                    //     let mut path1 = std::env::current_dir().expect("Couldn't get CWD!");
-                    //     path1.push("resources");
-                    //     path1.push("output");
-                    //     path1.push(format!("output_16.wav"));
-
-                    //     let mut file1 = OpenOptions::new().write(true).create(true).open(path1).unwrap();
-
-                    //     let bit_depth = wav::bit_depth::BitDepth::Sixteen(frame.iter().cloned().collect());
-                    //     let header1 = wav::Header::new(WAV_FORMAT_PCM, 1, 16_000, 16);
-                    //     wav::write(header1, &bit_depth, &mut file1).unwrap();
-                    // }
-            });
-        }
+    let consumer_handle = tokio::spawn(async move {
+        consumer_loop(&mut rx_48khz).await;
     });
 
     discord_handle.await.unwrap();
-    downsampler_handle.await.unwrap();
-    porcupine_handle.await.unwrap();
+    consumer_handle.await.unwrap();
+}
+
+async fn producer_loop() {
+
+
+
+    // Produce silence every 20ms
+    tokio::time::sleep(std::time::Duration::from_millis(20));
+}
+
+async fn consumer_loop(rx_48khz: &mut mpsc::Receiver<(u32, Vec<i16>)>) {
+    // Porcupine model per voice, should probably mix audio beforehand
+    let porcupine = init_porcupine();
+    let porcupine_sample_rate = porcupine.sample_rate();
+    let porcupine_frame_length = porcupine.frame_length();
+
+    // Cheetah
+    let cheetah = init_cheetah();
+    let cheetah_sample_rate = cheetah.sample_rate();
+    let cheetah_frame_length = cheetah.frame_length();
+
+    assert!(porcupine_sample_rate == cheetah_sample_rate);
+    assert!(cheetah_frame_length == porcupine_frame_length);
+
+    // Resampler
+    let resampler_params = SincInterpolationParameters {
+        sinc_len: 256,
+        f_cutoff: 0.95,
+        interpolation: SincInterpolationType::Linear,
+        oversampling_factor: 256,
+        window: WindowFunction::BlackmanHarris2,
+    };
+
+    let mut resampler = SincFixedOut::<f64>::new(
+        porcupine_sample_rate as f64 / 48_000 as f64,
+        2.0,
+        resampler_params,
+        porcupine_frame_length as usize,
+        2,
+    ).unwrap();
+
+    let mut buf = VecDeque::with_capacity(porcupine_frame_length as usize * 2);
+    let mut input_frame = Vec::<i16>::with_capacity(porcupine_frame_length as  usize);
+
+    let mut speech_to_text = false;
+
+    // Consume packets
+    loop {
+        let frames_needed = resampler.input_frames_next();
+        let frames = read_frames(rx_48khz, &mut buf, frames_needed, 2).await;
+
+        let result = resampler.process(&frames, None).unwrap();
+
+        input_frame.clear();
+        for i in 0..result[0].len() {
+            let l = result[0][i];
+            let r = result[1][i];
+            let v = (l + r) / 2.0;
+            input_frame.push((v * 32768.0).clamp(-32768.0, 32768.0) as i16);
+        }
+
+        // assert!(frame_length as usize == input_frame.len());
+
+        if speech_to_text {
+            if let Ok(cheetah_transcript) = cheetah.process(&input_frame) {
+                println!("Cheetah listening: {}", cheetah_transcript.transcript);
+                if cheetah_transcript.is_endpoint {
+                    if let Ok(cheetah_transcript) = cheetah.flush() {
+                        speech_to_text = false;
+                        println!("Cheetah finished: {}", cheetah_transcript.transcript)
+                    }
+                }
+            }
+        }
+        else {
+            // Listening in for trigger word
+            match porcupine.process(&input_frame) {
+                Ok(keyword_index) => {
+                    if keyword_index >= 0 {
+                        // Hit the trigger word, start speech to text
+                        speech_to_text = true;
+                        println!("bozo detected!");
+                    }
+                },
+                Err(e) => {
+                    println!("Porcupine error: {}", e);
+                }
+            }
+        }
+
+        // Write that shit
+        // if cringe.len() >= 16_000 * 5 {
+        //     let mut path1 = std::env::current_dir().expect("Couldn't get CWD!");
+        //     path1.push("resources");
+        //     path1.push("output");
+        //     path1.push(format!("output_16.wav"));
+
+        //     let mut file1 = OpenOptions::new().write(true).create(true).open(path1).unwrap();
+
+        //     let bit_depth = wav::bit_depth::BitDepth::Sixteen(cringe.iter().cloned().collect());
+        //     let header1 = wav::Header::new(WAV_FORMAT_PCM, 2, sample_rate, 16);
+        //     wav::write(header1, &bit_depth, &mut file1).unwrap();
+        //     cringe.clear();
+        // }
+    }
 }
 
 fn init_porcupine() -> Porcupine {
@@ -155,19 +184,21 @@ fn init_porcupine() -> Porcupine {
         .expect("Couldn't init porcupine!")
 }
 
-struct DiscordProducer {
-    prod: Producer<i16, Arc<SharedRb<i16, Vec<MaybeUninit<i16>>>>>,
-    //instant: Instant
+fn init_cheetah() -> Cheetah {
+    CheetahBuilder::new()
+        .access_key(env::var("PV_KEY").expect("Couldn't get env PV_KEY!"))
+        .init()
+        .expect("Unable to create Cheetah")
 }
 
 // Discord produces 48khz pcm 16
-struct DiscordHandles {
-    map: HashMap<u32, DiscordProducer>,
-    tx: mpsc::Sender<Consumer<i16, Arc<SharedRb<i16, Vec<MaybeUninit<i16>>>>>>
+struct SharedState {
+    tx: mpsc::Sender<(u32, Vec<i16>)>,
+    speaking: SpeakingUpdateData,
 }
 
-impl TypeMapKey for DiscordHandles {
-    type Value = DiscordHandles;
+impl TypeMapKey for SharedState {
+    type Value = SharedState;
 }
 
 #[group]
@@ -206,37 +237,8 @@ impl VoiceEventHandler for Receiver {
                     ssrc,
                     speaking,
                 );
-
-                let mut write_guard = self.data.write().await;
-                if let Some(buffers) = write_guard.get_mut::<DiscordHandles>() {
-                    // Create ring buffer prod/cons
-                    let (prod, cons) = ringbuf::HeapRb::<i16>::new(15 * 48_000).split();
-
-                    // Store the producer to insert packets into
-                    buffers.map.insert(ssrc.clone(), DiscordProducer {
-                        //instant: Instant::now(),
-                        prod: prod
-                    });
-
-                    // Send the consumer to read packets for processing
-                    buffers.tx.send(cons).await.unwrap();
-                }
             },
             EventContext::SpeakingUpdate(data) => {
-                // let mut write_guard = self.data.write().await;
-                //     if let Some(buffers) = write_guard.get_mut::<DiscordHandles>() {
-                //         if let Some(discord_prod) = buffers.map.get_mut(&data.ssrc) {
-                //             let silence_length = discord_prod.instant.elapsed().as_millis() * (48_000 / 1000);
-                //             discord_prod.instant = Instant::now();
-                //             for _ in 0..silence_length {
-                //                  if let Err(_) = discord_prod.prod.push(0) {
-                //                      tokio::time::sleep(Duration::from_millis(100)).await;
-                //                      break;
-                //                  }
-                //             }
-                //         }
-                //     }
-
                 println!(
                     "Source {} has {} speaking.",
                     data.ssrc,
@@ -254,17 +256,15 @@ impl VoiceEventHandler for Receiver {
                     );
 
                     let mut write_guard = self.data.write().await;
-                    if let Some(buffers) = write_guard.get_mut::<DiscordHandles>() {
-                        if let Some(discord_prod) = buffers.map.get_mut(&data.packet.ssrc) {
-                            // let silence_length = discord_prod.instant.elapsed().as_millis() * (48_000 / 1000);
-                            // discord_prod.instant = Instant::now();
-                            // for _ in 0..silence_length {
-                            //     if let Err(_) = discord_prod.prod.push(0) {
-                            //         tokio::time::sleep(Duration::from_millis(100)).await;
-                            //         break;
-                            //     }
-                            // }
-                            discord_prod.prod.push_iter(&mut audio.iter().cloned());
+                    if let Some(buffers) = write_guard.get_mut::<SharedState>() {
+
+                        match buffers.tx.send((data.packet.ssrc, audio.clone())).await {
+                            Ok(()) => {
+
+                            },
+                            Err(e) => {
+                                println!("Error {}", e);
+                            }
                         }
                     }
                 } else {
@@ -359,41 +359,6 @@ async fn leave(ctx: &Context, msg: &Message) -> CommandResult {
 
     Ok(())
 }
-
-// #[command]
-// #[only_in(guilds)]
-// async fn dump(ctx: &Context, _msg: &Message) -> CommandResult {
-
-//     let mut write_guard = ctx.data.write().await;
-
-//     if let Some(buffers) = write_guard.get_mut::<DiscordHandles>() {
-//         for (key, cons) in buffers.map {
-//             let mut path1 = std::env::current_dir().expect("Couldn't get CWD!");
-//             path1.push("resources");
-//             path1.push("output");
-//             path1.push(format!("output_16_{key}.wav"));
-
-//             let mut file1 = OpenOptions::new().write(true).create(true).open(path1).unwrap();
-
-//             let bit_depth = wav::bit_depth::BitDepth::Sixteen(cons.iter().cloned().collect());
-//             let header1 = wav::Header::new(WAV_FORMAT_PCM, 1, 16_000, 16);
-//             wav::write(header1, &bit_depth, &mut file1).unwrap();
-
-//             let mut path2 = std::env::current_dir().expect("Couldn't get CWD!");
-//             path2.push("resources");
-//             path2.push("output");
-//             path2.push(format!("output_48_{key}.wav"));
-
-//             let mut file2 = OpenOptions::new().write(true).create(true).open(path2).unwrap();
-
-//             let bit_depth = wav::bit_depth::BitDepth::Sixteen(cons.pcm_16_48.iter().cloned().collect());
-//             let header2 = wav::Header::new(WAV_FORMAT_PCM, 2, 48_000, 16);
-//             wav::write(header2, &bit_depth, &mut file2).unwrap();
-//         }
-//     }
-
-//     Ok(())
-// }
 
 async fn init_serenity() -> Client {
     let framework = StandardFramework::new()
