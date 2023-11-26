6c1722dc568506e87c2b34ffc98ff9b7985ec8bc
Tristan Colby <tristan.colb@gmail.com>
2023-11-26 13:48:35 -0800

    Kinda working finally!

---
+++ src/listener.rs
@@ -0,0 +1,169 @@
+use std::{fs::OpenOptions, env, collections::VecDeque};
+use tokio::sync::mpsc;
+use wav::WAV_FORMAT_PCM;
+
+// Resampling
+use rubato::{Resampler, SincFixedOut, SincInterpolationType, SincInterpolationParameters, WindowFunction};
+
+// APIs
+use cheetah::{CheetahBuilder, Cheetah};
+use porcupine::{PorcupineBuilder, Porcupine};
+use async_openai::{
+    types::{CreateSpeechRequestArgs, SpeechModel, Voice},
+    Client as OpenAIClient,
+};
+
+async fn read_frames(rx: &mut mpsc::Receiver<(u32, Vec<i16>)>, buf: &mut VecDeque<i16>, n: usize, channels: usize) -> Vec<Vec<f64>> {
+    let mut out = Vec::with_capacity(channels);
+    for _ in 0..channels {
+        out.push(Vec::with_capacity(n));
+    }
+
+    // TODO timeout waiting
+    while buf.len() < n * 2 {
+        let (_id, packet_audio) = rx.recv().await.unwrap();
+        buf.extend(packet_audio);
+    }
+    
+    for _ in 0..n {
+        let l = buf.pop_front().unwrap() as f64 / 32768.0;
+        let r = buf.pop_front().unwrap() as f64 / 32768.0;
+        out[0].push(l.clamp(-1.0, 1.0));
+        out[1].push(r.clamp(-1.0, 1.0));
+    }
+
+    out
+}
+
+
+fn init_porcupine() -> Porcupine {
+    let mut ppn_path = std::env::current_dir().expect("Couldn't get CWD!");
+    ppn_path.push("resources");
+    ppn_path.push("hey-bozo_en_windows_v2_2_0.ppn");
+
+    PorcupineBuilder::new_with_keyword_paths(env::var("PV_KEY").expect("Couldn't get env PV_KEY!"), &[ppn_path])
+        .init()
+        .expect("Couldn't init porcupine!")
+}
+
+fn init_cheetah() -> Cheetah {
+    CheetahBuilder::new()
+        .access_key(env::var("PV_KEY").expect("Couldn't get env PV_KEY!"))
+        .enable_automatic_punctuation(env::var("AUTO_PUNCTUATION").expect("Couldn't get env AUTO_PUNCTUATION!").parse().unwrap())
+        .endpoint_duration_sec(env::var("ENDPOINT_DURATION").expect("Couldn't get env ENDPOINT_DURATION!").parse().unwrap())
+        .init()
+        .expect("Unable to create Cheetah")
+}
+
+pub async fn listener_loop(rx_48khz: &mut mpsc::Receiver<(u32, Vec<i16>)>) {
+    let porcupine = init_porcupine();
+    let cheetah: Cheetah = init_cheetah();
+
+    assert!(porcupine.sample_rate() == cheetah.sample_rate());
+    assert!(cheetah.frame_length() == porcupine.frame_length());
+
+    let oai_client = OpenAIClient::new();
+
+    let resampler_params = SincInterpolationParameters {
+        sinc_len: 256,
+        f_cutoff: 0.95,
+        interpolation: SincInterpolationType::Linear,
+        oversampling_factor: 256,
+        window: WindowFunction::BlackmanHarris2,
+    };
+
+    let mut resampler = SincFixedOut::<f64>::new(
+        porcupine.sample_rate() as f64 / 48_000 as f64,
+        2.0,
+        resampler_params,
+        porcupine.frame_length() as usize,
+        2,
+    ).unwrap();
+
+    let mut buf = VecDeque::with_capacity(porcupine.frame_length() as usize * 2);
+    let mut input_frame = Vec::<i16>::with_capacity(porcupine.frame_length() as  usize);
+
+    let mut speech_to_text = false;
+    let mut total_transcript = String::new();
+
+    // TODO remove
+    let mut cringe = Vec::<i16>::with_capacity(16_000 * 10);
+
+    // Consume packets
+    loop {
+        let frames_needed = resampler.input_frames_next();
+        let frames = read_frames(rx_48khz, &mut buf, frames_needed, 2).await;
+
+        let resampled_frame = resampler.process(&frames, None).unwrap();
+
+        input_frame.clear();
+        for i in 0..resampled_frame[0].len() {
+            let l = resampled_frame[0][i];
+            let r = resampled_frame[1][i];
+            let v = (l + r) / 2.0;
+            input_frame.push((v * 32768.0).clamp(-32768.0, 32768.0) as i16);
+        }
+
+        if speech_to_text {
+
+            // TODO remove
+            cringe.extend(input_frame.clone());
+            
+            let partial_transcript = cheetah.process(&input_frame).unwrap();
+            total_transcript.push_str(&partial_transcript.transcript);
+
+            if partial_transcript.is_endpoint {
+                speech_to_text = false;
+
+                let final_transcript = cheetah.flush().unwrap();
+                total_transcript.push_str(&final_transcript.transcript);
+
+                println!("{}", total_transcript);
+
+                let voice_request: async_openai::types::CreateSpeechRequest = CreateSpeechRequestArgs::default()
+                   .input(total_transcript.clone())
+                   .voice(Voice::Onyx)
+                   .model(SpeechModel::Tts1)
+                   .build().unwrap();
+
+                match oai_client.audio().speech(voice_request).await {
+                    Ok(result) => {
+                        result.save("./resources/output/openai_output.mp3").await.unwrap();
+                    },
+                    Err(e) => {
+                        println!("{}", e.to_string());
+                        continue;
+                    } 
+                }
+
+                let mut path1 = std::env::current_dir().expect("Couldn't get CWD!");
+                path1.push("resources");
+                path1.push("output");
+                path1.push(format!("output_16.wav"));
+
+                let mut file1 = OpenOptions::new().write(true).create(true).open(path1).unwrap();
+
+                let bit_depth = wav::bit_depth::BitDepth::Sixteen(cringe.iter().cloned().collect());
+                let header1 = wav::Header::new(WAV_FORMAT_PCM, 1, porcupine.sample_rate(), 16);
+                wav::write(header1, &bit_depth, &mut file1).unwrap();
+            }
+        }
+        else {
+            // Listening in for trigger word
+            match porcupine.process(&input_frame) {
+                Ok(keyword_index) => {
+                    if keyword_index >= 0 {
+                        // Hit the trigger word, start speech to text
+                        speech_to_text = true;
+                        total_transcript.clear();
+                        cringe.clear();
+                        println!("bozo detected!");
+                    }
+                },
+                Err(e) => {
+                    println!("Porcupine error: {}", e);
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,19 @@
-use std::collections::VecDeque;
-use std::{env, sync::Arc};
-use cheetah::{CheetahBuilder, Cheetah};
-use porcupine::{PorcupineBuilder, Porcupine};
-use dotenv::dotenv;
+mod listener;
 
+use dotenv::dotenv;
+use songbird::events::context_data::SpeakingUpdateData;
+use tokio::time::Instant;
+use std::time::Duration;
+use std::{env, sync::Arc};
 use serenity::prelude::TypeMap;
 use serenity::{client::{Client, Context, EventHandler}, 
                framework::{StandardFramework, standard::{macros::{group, command}, Args, CommandResult}},
                prelude::{GatewayIntents, Mentionable, TypeMapKey},
                model::{gateway::Ready, prelude::{Message, ChannelId}}};
-use songbird::events::context_data::SpeakingUpdateData;
 use songbird::{EventHandler as VoiceEventHandler, EventContext, model::payload::{Speaking, ClientDisconnect}, Event, Config, driver::DecodeMode, SerenityInit, CoreEvent};
 use async_trait::async_trait;
 use tokio::sync::RwLock;
-use wav::WAV_FORMAT_PCM;
 use tokio::sync::mpsc;
-
-use rubato::{Resampler, SincFixedOut, SincInterpolationType, SincInterpolationParameters, WindowFunction};
-
-async fn read_frames(rx: &mut mpsc::Receiver<(u32, Vec<i16>)>, buf: &mut VecDeque<i16>, instant: &mut std::time::Instant, n: usize, channels: usize) -> Vec<Vec<f64>> {
-
-    let mut out = Vec::with_capacity(channels);
-    for _ in 0..channels {
-        out.push(Vec::with_capacity(n));
-    }
-
-    while buf.len() < n * 2 {
-        let (_id, packet_audio) = rx.recv().await.unwrap();
-        buf.extend(packet_audio);
-    }
-    
-    for _ in 0..n {
-        let l = buf.pop_front().unwrap() as f64 / 32768.0;
-        let r = buf.pop_front().unwrap() as f64 / 32768.0;
-        out[0].push(l.clamp(-1.0, 1.0));
-        out[1].push(r.clamp(-1.0, 1.0));
-    }
-
-    out
-}
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() {
@@ -47,13 +22,14 @@ async fn main() {
     
     let mut client = init_serenity().await;
 
-    //let audio_rb = ringbuf::HeapRb::<i32>::new(64);
-
+    let (tx_speaking_state, mut rx_speaking_state) = mpsc::channel::<SpeakingUpdateData>(32);
     let (tx_48khz, mut rx_48khz) = mpsc::channel::<(u32, Vec<i16>)>(32);
+
     {
         let mut guard: tokio::sync::RwLockWriteGuard<'_, serenity::prelude::TypeMap> = client.data.write().await;
         guard.insert::<SharedState>(SharedState {
-            tx: tx_48khz
+            tx_packet: tx_48khz.clone(),
+            tx_state: tx_speaking_state.clone()
         });
     };
 
@@ -61,140 +37,41 @@ async fn main() {
         client.start().await.expect("Error in serenity client!");
     });
 
-    let consumer_handle = tokio::spawn(async move {
-        consumer_loop(&mut rx_48khz).await;
+    let consumer_handle: tokio::task::JoinHandle<()> = tokio::spawn(async move {
+        listener::listener_loop(&mut rx_48khz).await;
+    });
+
+    let producer_handle = tokio::spawn(async move {
+        producer_loop(&mut rx_speaking_state, &mut tx_48khz.clone()).await;
     });
 
     discord_handle.await.unwrap();
     consumer_handle.await.unwrap();
+    producer_handle.await.unwrap();
 }
 
-async fn producer_loop() {
-
-
-
-    // Produce silence every 20ms
-    tokio::time::sleep(std::time::Duration::from_millis(20));
-}
-
-async fn consumer_loop(rx_48khz: &mut mpsc::Receiver<(u32, Vec<i16>)>) {
-    // Porcupine model per voice, should probably mix audio beforehand
-    let porcupine = init_porcupine();
-    let porcupine_sample_rate = porcupine.sample_rate();
-    let porcupine_frame_length = porcupine.frame_length();
-
-    // Cheetah
-    let cheetah = init_cheetah();
-    let cheetah_sample_rate = cheetah.sample_rate();
-    let cheetah_frame_length = cheetah.frame_length();
-
-    assert!(porcupine_sample_rate == cheetah_sample_rate);
-    assert!(cheetah_frame_length == porcupine_frame_length);
-
-    // Resampler
-    let resampler_params = SincInterpolationParameters {
-        sinc_len: 256,
-        f_cutoff: 0.95,
-        interpolation: SincInterpolationType::Linear,
-        oversampling_factor: 256,
-        window: WindowFunction::BlackmanHarris2,
-    };
-
-    let mut resampler = SincFixedOut::<f64>::new(
-        porcupine_sample_rate as f64 / 48_000 as f64,
-        2.0,
-        resampler_params,
-        porcupine_frame_length as usize,
-        2,
-    ).unwrap();
-
-    let mut buf = VecDeque::with_capacity(porcupine_frame_length as usize * 2);
-    let mut input_frame = Vec::<i16>::with_capacity(porcupine_frame_length as  usize);
-
-    let mut speech_to_text = false;
-
-    // Consume packets
+async fn producer_loop(rx_state: &mut mpsc::Receiver<SpeakingUpdateData>, tx_48khz: &mut mpsc::Sender<(u32, Vec<i16>)>) {
+    // TODO handle this via start/stop speaking
+    let mut stopped = Instant::now();
     loop {
-        let frames_needed = resampler.input_frames_next();
-        let frames = read_frames(rx_48khz, &mut buf, frames_needed, 2).await;
-
-        let result = resampler.process(&frames, None).unwrap();
-
-        input_frame.clear();
-        for i in 0..result[0].len() {
-            let l = result[0][i];
-            let r = result[1][i];
-            let v = (l + r) / 2.0;
-            input_frame.push((v * 32768.0).clamp(-32768.0, 32768.0) as i16);
-        }
-
-        // assert!(frame_length as usize == input_frame.len());
-
-        if speech_to_text {
-            if let Ok(cheetah_transcript) = cheetah.process(&input_frame) {
-                println!("Cheetah listening: {}", cheetah_transcript.transcript);
-                if cheetah_transcript.is_endpoint {
-                    if let Ok(cheetah_transcript) = cheetah.flush() {
-                        speech_to_text = false;
-                        println!("Cheetah finished: {}", cheetah_transcript.transcript)
-                    }
-                }
+        let state = rx_state.recv().await.unwrap();
+        if state.speaking {
+            let mut silence_duration = stopped.elapsed();
+            if silence_duration > Duration::from_millis(3000) {
+                silence_duration = Duration::from_millis(3000);
             }
+            let sample_count = (silence_duration.as_millis() * 48) as usize;
+            tx_48khz.send((0, vec![0; sample_count])).await.unwrap();
+        } else {
+            stopped = Instant::now();
         }
-        else {
-            // Listening in for trigger word
-            match porcupine.process(&input_frame) {
-                Ok(keyword_index) => {
-                    if keyword_index >= 0 {
-                        // Hit the trigger word, start speech to text
-                        speech_to_text = true;
-                        println!("bozo detected!");
-                    }
-                },
-                Err(e) => {
-                    println!("Porcupine error: {}", e);
-                }
-            }
-        }
-
-        // Write that shit
-        // if cringe.len() >= 16_000 * 5 {
-        //     let mut path1 = std::env::current_dir().expect("Couldn't get CWD!");
-        //     path1.push("resources");
-        //     path1.push("output");
-        //     path1.push(format!("output_16.wav"));
-
-        //     let mut file1 = OpenOptions::new().write(true).create(true).open(path1).unwrap();
-
-        //     let bit_depth = wav::bit_depth::BitDepth::Sixteen(cringe.iter().cloned().collect());
-        //     let header1 = wav::Header::new(WAV_FORMAT_PCM, 2, sample_rate, 16);
-        //     wav::write(header1, &bit_depth, &mut file1).unwrap();
-        //     cringe.clear();
-        // }
     }
-}
-
-fn init_porcupine() -> Porcupine {
-    let mut ppn_path = std::env::current_dir().expect("Couldn't get CWD!");
-    ppn_path.push("resources");
-    ppn_path.push("hey-bozo_en_windows_v2_2_0.ppn");
-
-    PorcupineBuilder::new_with_keyword_paths(env::var("PV_KEY").expect("Couldn't get env PV_KEY!"), &[ppn_path])
-        .init()
-        .expect("Couldn't init porcupine!")
-}
-
-fn init_cheetah() -> Cheetah {
-    CheetahBuilder::new()
-        .access_key(env::var("PV_KEY").expect("Couldn't get env PV_KEY!"))
-        .init()
-        .expect("Unable to create Cheetah")
 }
 
 // Discord produces 48khz pcm 16
 struct SharedState {
-    tx: mpsc::Sender<(u32, Vec<i16>)>,
-    speaking: SpeakingUpdateData,
+    tx_packet: mpsc::Sender<(u32, Vec<i16>)>,
+    tx_state: mpsc::Sender<SpeakingUpdateData>
 }
 
 impl TypeMapKey for SharedState {
@@ -244,6 +121,11 @@ impl VoiceEventHandler for Receiver {
                     data.ssrc,
                     if data.speaking {"started"} else {"stopped"},
                 );
+
+                let mut write_guard = self.data.write().await;
+                if let Some(state) = write_guard.get_mut::<SharedState>() {
+                    state.tx_state.send(data.clone()).await.unwrap();
+                }
             },
             EventContext::VoicePacket(data) => {
                 if let Some(audio) = data.audio {
@@ -256,9 +138,9 @@ impl VoiceEventHandler for Receiver {
                     );
 
                     let mut write_guard = self.data.write().await;
-                    if let Some(buffers) = write_guard.get_mut::<SharedState>() {
+                    if let Some(state) = write_guard.get_mut::<SharedState>() {
 
-                        match buffers.tx.send((data.packet.ssrc, audio.clone())).await {
+                        match state.tx_packet.send((data.packet.ssrc, audio.clone())).await {
                             Ok(()) => {
 
                             },
