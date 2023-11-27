5f807511890ce490e61ddd25a81a08ea42ab154b
Tristan Colby <tristan.colb@gmail.com>
2023-11-26 18:23:58 -0800

    Multi user support

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-mod listener;
 mod discord;
+mod listener;
 
+use std::collections::HashMap;
 use dotenv::dotenv;
-use tokio::sync::mpsc;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 10)]
 async fn main() {
@@ -11,13 +11,12 @@ async fn main() {
     
     let mut client = discord::init_serenity().await;
 
-    let (tx_48khz, mut rx_48khz) = mpsc::channel::<(u32, Vec<i16>)>(32);
-
     {
         // Initialize shared state.
         let mut guard: tokio::sync::RwLockWriteGuard<'_, serenity::prelude::TypeMap> = client.data.write().await;
         guard.insert::<discord::SharedState>(discord::SharedState {
-            tx_packet: tx_48khz.clone()
+            users: HashMap::default(),
+            id_to_ssrc: HashMap::default()
         });
     };
 
@@ -25,10 +24,5 @@ async fn main() {
         client.start().await.expect("Error in serenity client!");
     });
 
-    let consumer_handle = tokio::spawn(async move {
-        listener::listener_loop(&mut rx_48khz).await;
-    });
-
     discord_handle.await.unwrap();
-    consumer_handle.await.unwrap();
 }
