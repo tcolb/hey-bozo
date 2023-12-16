f9987c89b65a62b82e25b2472d3e717c835643ba
Tristan Colby <tristan.colb@gmail.com>
2023-12-16 01:36:39 -0800

    Warning cleanup

--- src/action_handler.rs
+++ src/action_handler.rs
@@ -13,31 +13,31 @@ pub async fn action_handler_loop(http: Arc<Http>, channel: ChannelId, mut action
             AssistantAction::MusicBot(music_bot_action) => {
                 match music_bot_action {
                     MusicBotAction::Summon => {
-                        channel.say(&http, "=join").await;
+                        channel.say(&http, "=join").await.unwrap();
                     }
                     MusicBotAction::Dismiss => {
-                        channel.say(&http, "=leave").await;
+                        channel.say(&http, "=leave").await.unwrap();
                     }
                     MusicBotAction::Request(title) => {
-                        channel.say(&http, format!("=p {}", title)).await;
+                        channel.say(&http, format!("=p {}", title)).await.unwrap();
                     }
                     MusicBotAction::Skip => {
-                        channel.say(&http, "=skip").await;
+                        channel.say(&http, "=skip").await.unwrap();
                     }
                     MusicBotAction::Shuffle => {
-                        channel.say(&http, "=shuffle").await;
+                        channel.say(&http, "=shuffle").await.unwrap();
                     }
                     MusicBotAction::Loop => {
-                        channel.say(&http, "=loop").await;
+                        channel.say(&http, "=loop").await.unwrap();
                     }
                     MusicBotAction::Clear => {
-                        channel.say(&http, "=clear").await;
+                        channel.say(&http, "=clear").await.unwrap();
                     }
                     MusicBotAction::BassBoost => {
-                        channel.say(&http, "=bb").await;
+                        channel.say(&http, "=bb").await.unwrap();
                     }
                     MusicBotAction::PlayPlaylist(playlist) => {
-                        channel.say(&http, format!("=playlist play {}", playlist)).await;
+                        channel.say(&http, format!("=playlist play {}", playlist)).await.unwrap();
                     }
                 }
             }
